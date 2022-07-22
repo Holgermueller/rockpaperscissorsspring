0f43953c9c0d70dd148aaeaceec858913ca6c310
Holger Mueller <9jholger@gmail.com>
2022-07-22 12:19:07 -0500

    some code

---
+++ src/main/java/com/holgermueller/rockpaperscissorsspring/Game.java
@@ -0,0 +1,15 @@
+package com.holgermueller.rockpaperscissorsspring;
+
+public class Game {
+    private String userInput;
+    private String computerChoice;
+
+    public String getUserInput() {
+        return userInput;
+
+    }
+
+    public String getComputerChoice() {
+        return computerChoice;
+    }
+}
---
+++ src/main/java/com/holgermueller/rockpaperscissorsspring/GameController.java
@@ -0,0 +1,19 @@
+package com.holgermueller.rockpaperscissorsspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GameController {
+
+    // @GetMapping("/")
+    // public String Game(Model model) {
+
+    // Game game = new Game();
+    // model.addAttribute("game", game);
+
+    // return
+    // }
+
+}
