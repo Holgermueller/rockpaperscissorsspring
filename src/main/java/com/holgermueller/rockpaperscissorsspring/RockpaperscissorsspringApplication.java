32bd4a2484832320e4facb7175d85e31265d18b2
Holger Mueller <9jholger@gmail.com>
2022-07-22 10:40:57 -0500

    first commit

---
+++ src/main/java/com/holgermueller/rockpaperscissorsspring/RockpaperscissorsspringApplication.java
@@ -0,0 +1,13 @@
+package com.holgermueller.rockpaperscissorsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockpaperscissorsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockpaperscissorsspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/holgermueller/rockpaperscissorsspring/RockpaperscissorsspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holgermueller.rockpaperscissorsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockpaperscissorsspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
