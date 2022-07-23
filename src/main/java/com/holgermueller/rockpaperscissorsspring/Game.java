3300a763e974df7bcc0ef83417acbc50c2e4a631
Holger Mueller <9jholger@gmail.com>
2022-07-23 10:53:21 -0500

    got form on page

--- src/main/java/com/holgermueller/rockpaperscissorsspring/Game.java
+++ src/main/java/com/holgermueller/rockpaperscissorsspring/Game.java
@@ -1,5 +1,13 @@
 package com.holgermueller.rockpaperscissorsspring;
 
 public class Game {
+    private String userChoice;
 
+    public String getUserChoice() {
+        return userChoice;
+    }
+
+    public void setUserChoice(String userChoice) {
+        this.userChoice = userChoice;
+    }
 }
--- src/main/java/com/holgermueller/rockpaperscissorsspring/GameController.java
+++ src/main/java/com/holgermueller/rockpaperscissorsspring/GameController.java
@@ -1,14 +1,24 @@
 package com.holgermueller.rockpaperscissorsspring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GameController {
 
     @GetMapping("/game")
-    public String game() {
+    public String gameForm(Model model) {
+        model.addAttribute("game", new Game());
         return "game";
     }
+
+    // @PostMapping("/game")
+    // public String gameSubmit(@ModelAttribute Game game, Model model) {
+    // model.addAttribute("game", game);
+    // return game;
+    // }
 
 }
