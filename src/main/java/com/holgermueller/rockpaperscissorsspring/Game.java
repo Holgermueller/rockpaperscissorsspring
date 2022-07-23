d0893166556c090f135f1a16b22acdcb53ac525c
Holger Mueller <9jholger@gmail.com>
2022-07-23 00:21:42 -0500

    starting from scratch

--- src/main/java/com/holgermueller/rockpaperscissorsspring/Game.java
+++ src/main/java/com/holgermueller/rockpaperscissorsspring/Game.java
@@ -1,15 +1,5 @@
 package com.holgermueller.rockpaperscissorsspring;
 
 public class Game {
-    private String userInput;
-    private String computerChoice;
 
-    public String getUserInput() {
-        return userInput;
-
-    }
-
-    public String getComputerChoice() {
-        return computerChoice;
-    }
 }
