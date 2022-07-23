a4e7672db302908d1d2c1f87e41a0dd903890135
Holger Mueller <9jholger@gmail.com>
2022-07-23 10:32:13 -0500

    got page to redirect

--- src/main/java/com/holgermueller/rockpaperscissorsspring/GameController.java
+++ src/main/java/com/holgermueller/rockpaperscissorsspring/GameController.java
@@ -1,7 +1,14 @@
 package com.holgermueller.rockpaperscissorsspring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class GameController {
+
+    @GetMapping("/game")
+    public String game() {
+        return "game";
+    }
 
 }
